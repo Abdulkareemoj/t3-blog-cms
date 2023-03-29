3219c22d39dc0e1f8a84a5c47f06175983ed193b
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-29 09:36:05 +0100

    nothing here

---
+++ simple-t3blog/src/pages/components/Navbar.tsx
@@ -0,0 +1,36 @@
+import {useContext} from 'react'
+import Link from "next/link";
+import Categories from './Categories';
+
+const categories = [
+    {name: 'Technology', slug: 'technology'},
+    {name: 'Business', slug: 'business'},
+
+]
+const Navbar = () => {
+  return (
+    <div className='container mx-auto px-10 mb-8'>Navbar
+   <div className='border-b w-full inline-block border-blue-400 py-8 '>
+    <div className='md:float-left block'>
+        <Link href='/'>
+            <span className='cursor-pointer font-bold text-4xl text-white'>
+                BlogCMS 
+            </span>
+   </Link>
+    </div>
+<div className='hidden md:float-left md:contents'>
+    {categories.map((category) => (
+        <Link key={category.slug}  href={`/category/${category.slug}`}>
+            <span className='cursor-pointer md:float-right text-white text-xl font-bold ml-4'>
+                {category.name}
+            </span>
+
+        </Link>
+   ) )}
+</div>
+   </div>
+    </div>
+  )
+}
+
+export default Navbar
