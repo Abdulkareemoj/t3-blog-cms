5fd0f349a61d7c8b50b3bc7a02219b24778a8d0e
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-30 23:29:47 +0100

    add schema

--- simple-t3blog/services/index.ts
+++ simple-t3blog/services/index.ts
@@ -1,8 +1,38 @@
+import { graphql } from 'graphql'
 import {request, gql} from 'graphql-request'
 
 export const getPosts = async ()  =>{
-    const query =gql
+    const query = gql`
     query MyQuery{
-        
+         
+  postsConnection {
+    edges {
+      node {
+        authors {
+          id
+          description
+          name
+          image {
+            url
+          }
+          createdAt
+        }
+        slug
+        title
+        description
+        featuredImage {
+          url
+        }
+        categories {
+          slug
+          categoryName
+        }
+      }
     }
+  }
+    }`
 }
+
+const results = await request(graphqlAPI, query)
+
+return result.postsConnection.edges
