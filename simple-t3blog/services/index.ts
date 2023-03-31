39a026c5d941eed79db06bf0a07c093cbae1c41a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-31 22:31:09 +0100

    reworked query

--- simple-t3blog/services/index.ts
+++ simple-t3blog/services/index.ts
@@ -5,37 +5,37 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 export const getPosts = async ()  => {
     const query = gql`
-    query MyQuery{
-         
-  postsConnection {
-    edges {
-      node {
-        authors {
-          id
-          description
-          name
-          image {
-            url
-          }
-          createdAt
-        }
-        slug
-        title
-        description
-        featuredImage {
-          url
-        }
-        categories {
-          slug
-          categoryName
-        }
-      }
-    }
-  }
-
+    query MyQuery{       
+        postsConnection {
+            edges {
+                node {
+                    authors {
+                        id
+                        description
+                        name
+                        image {
+                                url
+                            }
+                        }
+                    createdAt
+                    slug
+                    title
+                    description
+                    featuredImage {
+                        url
+                        }
+                    categories {
+                        slug
+                        categoryName
+                        }
+                    }
+                }
+            }
 }
 
-;`
+;
+
+`
 
 const result = await request(graphqlAPI, query)
 
