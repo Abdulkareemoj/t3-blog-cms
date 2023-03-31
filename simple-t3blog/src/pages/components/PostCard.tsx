04d97f013cebe7f622d6fe8fa2cf935e7f8253af
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-03-31 21:42:51 +0100

    created postcard component

--- simple-t3blog/src/pages/components/PostCard.tsx
+++ simple-t3blog/src/pages/components/PostCard.tsx
@@ -1,7 +1,14 @@
+import moment from "moment"
+import Image from "next/image"
 
-const PostCard = () => {
+
+const PostCard = ( {post} ) => {
   return (
-    <div>PostCard</div>
+    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
+       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
+      <Image src="{post.featuredImage.url}" alt="{post.title}" className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg" />
+      </div>
+    </div>
   )
 }
 
