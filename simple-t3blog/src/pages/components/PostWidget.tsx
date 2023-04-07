a8c4761960bb31727357895b228b2ee806a1dcb8
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-07 22:19:15 +0100

    add function

--- simple-t3blog/src/pages/components/PostWidget.tsx
+++ simple-t3blog/src/pages/components/PostWidget.tsx
@@ -1,10 +1,31 @@
 import moment from "moment"
 import Link from "next/link"
-
+import { useState, useEffect  } from "react"
+import { getRecentPosts, getSimilarPosts } from "services"
 
 const PostWidget = () => {
   const [relatedPosts, setRelatedPosts] = useState([])
-  
+  useEffect(()=>{
+    if (slug) {
+      getSimilarPosts(category, slug)
+      .then((result) => setRelatedPosts(result))
+    }
+    else{
+      getRecentPosts(category, slug)
+      .then((result) => setRelatedPosts(result))
+    }
+  }
+
+
+//   useEffect(()=>{
+//     slug ? getSimilarPosts(category, slug)
+//     .then((result) => setRelatedPosts(result)) : getRecentPosts(category, slug)
+//     .then((result) => setRelatedPosts(result));
+// }
+
+
+
+  )
   return (
     <div>PostWidget</div>
   )
