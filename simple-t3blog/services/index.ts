c58821f6f843e0e0bb1eaf06864de3b8fbce553a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-08 17:00:37 +0100

    update queries

--- simple-t3blog/services/index.ts
+++ simple-t3blog/services/index.ts
@@ -1,38 +1,38 @@
 import {request, gql} from 'graphql-request'
+import Categories from '../components/Categories';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 export const getPosts = async ()  => {
-    const query = gql`
-    query MyQuery{       
-        postsConnection {
-            edges {
-                node {
-                    authors {
-                        id
-                        description
-                        name
-                        image {
-                                url
-                            }
-                        }
-                    createdAt
-                    slug
-                    title
-                    description
-                    featuredImage {
-                        url
-                        }
-                    categories {
-                        slug
-                        categoryName
-                        }
-                    }
-                }
-            }
+const query = gql`
+query MyQuery{       
+postsConnection {
+edges {
+node {
+authors {
+id
+description
+name
+image {
+url
+}
+}
+createdAt
+slug
+title
+description
+featuredImage {
+url
+}
+categories {
+slug
+categoryName
+}
+}
+}
+}
 }
 
-;
 
 `
 
@@ -42,44 +42,58 @@ return result.postsConnection.edges
 }
 
 export const getRecentPosts = async ()  => {
-    const query = gql`
-    query getPostDetails() {
-        posts(
-            orderBy: createdAt_ASC,
-            last: 3) {
-            
-                    createdAt
-                    slug
-                    title
-                    featuredImage {
-                        url
-                    }
-        }
-    }
+const query = gql`
+query getPostDetails() {
+posts(
+orderBy: createdAt_ASC,
+last: 3) {
+
+createdAt
+slug
+title
+featuredImage {
+url
+}
+}
+}
 ` 
-    const result = await request(graphqlAPI, query)
-    return result.posts
+const result = await request(graphqlAPI, query)
+return result.posts
 
 }
-    
+
 
 
 export const getSimilarPosts = async ()  => {
-    const query = gql`
-    query getPostDetails ($slug: String! $categories: [String!]){
-       posts(
-        where: { slug_not: $slug, AND {categories_some {slug+in: $categories}}}
-        last: 3
-       ){
-        createdAt
-                    slug
-                    title
-                    featuredImage {
-                        url
-                    }
-       }
-         }
+const query = gql`
+query getPostDetails ($slug: String! $categories: [String!]){
+posts(
+where: { slug_not: $slug, AND {categories_some {slug+in: $categories}}}
+last: 3
+){
+createdAt
+slug
+title
+featuredImage {
+url
+}
+}
+}
 ` 
-    const result = await request(graphqlAPI, query)
-    return result.posts
+const result = await request(graphqlAPI, query)
+return result.posts
+}
+
+
+export const getCategories = async ()  => {
+const query = gql`
+query GetCategories{
+categories{
+name
+slug
+}
+}
+` 
+const result = await request(graphqlAPI, query)
+return result.categories
 }
