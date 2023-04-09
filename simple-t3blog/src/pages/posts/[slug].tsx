0e7db1636b13807e7ab89c4febc87f56b0fb013c
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-09 22:51:16 +0100

    added slug

---
+++ simple-t3blog/src/pages/posts/[slug].tsx
@@ -0,0 +1,11 @@
+import { getPosts, getPostDetails } from "services"
+import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from "components"
+const PostDetails = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostDetails
