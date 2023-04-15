241b2f1582c2e3874f2efb0e11bcb86104ce05eb
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-15 22:47:34 +0100

    linting issues

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,24 +1,38 @@
-import { getPosts, getPostDetails } from "services"
-import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from "components"
+import { getPosts, getPostDetails } from "services";
+import {
+  PostDetail,
+  Categories,
+  PostWidget,
+  Author,
+  Comments,
+  CommentsForm,
+} from "components";
 const PostDetails = () => {
   return (
-    <div className="container mx-auto px-10 mb-10">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-<div className="col-span-1 lg:col-span-8">
- <PostDetail />
- <Author />
- <CommentsForm />
- <Comments />
-</div>
-<div className="col-span-1 lg:col-span-4">
-  <div className="relative lg:sticky top-8">
-  <PostWidget />
-  <Categories />
-  </div>
-</div>
+    <div className="container mx-auto mb-10 px-10">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          <PostDetail />
+          <Author />
+          <CommentsForm />
+          <Comments />
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostDetails
+export default PostDetails;
+
+export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug);
+  return {
+    props: { posts: data },
+  };
+}
