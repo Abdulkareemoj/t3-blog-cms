fe28a8856e0c616777c3d11e611ebf2bd5776c12
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-17 22:16:40 +0100

    add params

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,4 +1,5 @@
 import { getPosts, getPostDetails } from "services";
+import Categories from '../../../types/Categories';
 import {
   PostDetail,
   Categories,
@@ -12,14 +13,14 @@ const PostDetails = () => {
     <div className="container mx-auto mb-10 px-10">
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
-          <PostDetail />
-          <Author />
-          <CommentsForm />
-          <Comments />
+          <PostDetail post={post}/>
+          <Author author={post.author}/>
+          <CommentsForm slug={post.slug}/>
+          <Comments slug={post.slug}/>
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative top-8 lg:sticky">
-            <PostWidget />
+            <PostWidget category={post.Categories.map((category)=> category.slug)} slug={post.slug} />
             <Categories />
           </div>
         </div>
@@ -31,6 +32,14 @@ const PostDetails = () => {
 export default PostDetails;
 
 export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug);
+  return {
+    props: { posts: data },
+  };
+}
+
+
+export async function getStaticPaths() {
   const data = await getPostDetails(params.slug);
   return {
     props: { posts: data },
