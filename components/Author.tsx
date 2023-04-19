0c8cbaa451a54b07201b2dc0c93e78ac6964b427
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-19 17:02:58 +0100

    feat: :sparkles: completed author component

--- components/Author.tsx
+++ components/Author.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import Image from 'next/image';
 
-const Author = () => {
-  return <div></div>;
+const Author = ( {author}) => {
+  return(
+   <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-opacity-20">
+ <div className="absolute left-0 right-2 top-14">  
+   <Image
+    alt={author.name}
+    unoptimized
+    height={100}
+    width={100}
+    src={author.photo.url}
+    className='align-middle rounded-full'
+    /> 
+    </div>
+    <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+    <p className="text-white text-lg">{author.bio}</p>
+   </div>
+      )
 };
 
 export default Author;
