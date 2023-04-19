95d90888d6c016a237793d562ad828f432f53864
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-19 17:04:08 +0100

    feat: :sparkles: completed postdetais component

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -2,6 +2,48 @@ import moment from 'moment';
 import Image from 'next/image';
 
 const PostDetail = ( {post} ) => {
+  const getContentFragment = (index, text, obj, type) =>{
+let modifiedText: string = text
+
+if (obj){
+  if(obj.bold){
+    modifiedText = (<b key={index}>{text}</b>)
+  }
+  if(obj.italic){
+    modifiedText = (<em key={index}>{text}</em>)
+  }
+  if(obj.underline){
+    modifiedText = (<u key={index}>{text}</u>)
+  }
+}
+
+switch(type){
+  case 'heading-three':
+return <h3 key={index} className='text-xl font-semibold mb-4'></h3>
+
+case 'paragraph':
+  return <p key={index} className='mb-8'>{modifiedText.map((item))}</p>
+  
+  case 'heading-four':
+return <h4 key={index} className='text-xl font-semibold mb-4'></h4>
+
+case 'image':
+  return (
+  <Image 
+  key={index}
+  alt={obj.title}
+  height={obj.height}
+  width={obj.width}
+  src={obj.src}
+  />
+  )
+  default:
+  return modifiedText
+}
+
+  }
+
+
   return (
     <div>
 <div className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
@@ -41,7 +83,11 @@ const PostDetail = ( {post} ) => {
   {post.title}
 
 </h1>
-{post.content.raw.children.map((typeObj))}
+{post.content.raw.children.map((typeObj, index => {
+  const children =typeObj.childres.map((item, itemIndex) => getContentFragment(itemIndex, item.text, item)) 
+  return getContentFragment(index, children, typeObj, typeObj.type)
+}))}
+
   </div>
 </div>
     </div>
