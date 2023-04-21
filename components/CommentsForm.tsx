e61afa4ce6a3b4641cc4aef8782bc1415fdfd368
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-21 21:42:24 +0100

    comments box

--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -9,8 +9,11 @@ const CommentsForm = () => {
    const nameEl= useRef()
    const emailEl= useRef()
    const storeDataEl= useRef()
+
   return (
-  <div></div>
+  <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+    <h3 className="text-xl mb-8 font-semibold border-b pb-4">Comments</h3>
+  </div>
     );
 };
 
