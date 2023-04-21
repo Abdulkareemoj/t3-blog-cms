8425a68cfa364149e21cceb2dce004fa5ae6ab3d
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-21 21:39:21 +0100

    add refs

--- components/CommentsForm.tsx
+++ components/CommentsForm.tsx
@@ -1,10 +1,14 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 
 const CommentsForm = () => {
    const [error, setError] = useState(false)
    const [localStorage, setLocalStorage] = useState(false)
    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
 
+   const commentEl = useRef()
+   const nameEl= useRef()
+   const emailEl= useRef()
+   const storeDataEl= useRef()
   return (
   <div></div>
     );
