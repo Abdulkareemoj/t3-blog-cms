3273aeb97184b811dfcd47be71b27debbea9fdf7
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-28 09:11:39 +0100

    feat: :sparkles:

--- src/pages/api/comment.tsx
+++ src/pages/api/comment.tsx
@@ -4,6 +4,17 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export default function comment(res: any, req: any){
     const graphQLClient = new GraphQLClient(graphqlAPI, {
+headers:{
+    authorization: `Bearer ${process.env.GRAPHCMS_TOKEN}`
+}
+    })
+    const query = gql`
+    mutation CreateComment($comment: String!, $email: String!, $name: String!, $slug: String!){
+        createComment(data: {comment: $comment, email: $email, name: $name, post:{connect: {slug: $slug}}}){
+            id
+        }
 
-    }
+       }   `
+       const result = await graphQLClient.request(query, req.body){
+        return res.status(200).send(result)
 }
