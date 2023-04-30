29ba4fbd5fc144d30eac3370c5d6fc536c511bac
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-04-30 08:59:48 +0100

    done for now

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,26 +1,42 @@
-import moment from "moment";
-import parse from "html-react-parser";
-import {useState, useEffect} from "react";
-import { getComments } from "services";
-
+import moment from 'moment'
+import parse from 'html-react-parser'
+import { useState, useEffect } from 'react'
+import { getComments } from 'services'
 
 const Comments = () => {
-  useEffect(() => {
-    getComments(slug)
-    .then((result) => setComments(result)
-  }, []); 
+    const [comments, setComments] = useState([])
+    useEffect(() => {
+        getComments(slug).then((result) => setComments(result))
+    }, [])
 
-  return (
-  <div>
-    comment.length > 0 && (
-      <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-<h3 className="text-xl mb-8 font-semibold border-b pb-4">
-
-</h3>
-      </div>
+    return (
+        <div>
+            {comment.length > 0 && (
+                <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+                    <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+                        {comments.length}{' '}
+                    </h3>
+                    {comments.map((comment) => (
+                        <div
+                            key={comment.createdAt}
+                            className="mb-4 border-b border-gray-100 pb-4"
+                        >
+                            <p className="mb-4">
+                                <span className="font-semibold">
+                                    {comment.name}
+                                </span>{' '}
+                                on{' '}
+                                {moment(comment.createdAt).format('MM DD,YYYY')}
+                            </p>
+                            <p className="w-full whitespace-pre-line text-gray-100">
+                                {parse(comment.comment)}
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
     )
-  </div>
-  );
-};
+}
 
-export default Comments;
+export default Comments
