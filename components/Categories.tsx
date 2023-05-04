eb05a806a28632d9d7481f76c07e7ad080a14cb0
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2023-05-04 15:39:09 +0100

    fixed promise statement

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -2,12 +2,16 @@ import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { getCategories } from 'services'
 
-import { Categories } from '../types/Categories'
+// import { Categories } from '../types/Categories'
 
 const Categories = () => {
     const [categories, setCategories] = useState([])
     useEffect(() => {
-        getCategories().then((newCategories) => setCategories(newCategories))
+        getCategories()
+            .then((newCategories) => setCategories(newCategories))
+            .catch((e) => {
+                console.error(e)
+            })
     }, [])
 
     //  const category = ({ categories }: categories) => {
